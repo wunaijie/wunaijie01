b5091ac409bb955a6b765a475c5201c3c99dca95
wunaijie <2375982579@qq.com>
2019-05-27 11:03:19 +0800

    我的项目

--- case/test02.py
+++ case/test02.py
@@ -3,6 +3,7 @@ import allure
 
 class Test02(object):
     def test02(self):
+        print("test02")
         """添加图片进测试报告"""
-        with open("C:\\Users\\Administrator\\Desktop\\day07\\.pytest_cache\\自己画的PO流程png.png", "rb") as f:
+        with open("C:\\Users\\Administrator\\Desktop\\day07\\image\\自己画的PO流程png.png", "rb") as f:
             allure.attach("截图", f.read(), allure.attach_type.PNG)
